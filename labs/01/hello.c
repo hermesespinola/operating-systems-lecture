d0de9ffe65d0e440bdb4cebd3c5c034c913cc037
Adler Alonso <a01630908@itesm.mx>
2017-08-14 07:47:48 -0500

    A01630908-homework-01

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -4,6 +4,6 @@
 
 int main(void) {
     printf("Hello World\n");
-    printf("My name is Christopher Salvador Márquez Álvarez \n");
+    printf("My name is Christopher Adler Alonso Zamora Ruíz \n");
     return 0; 
 }
